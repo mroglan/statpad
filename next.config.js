9c3633a58887795fbf68c8f21d66d86a1a13d9d9
mroglan <mroglan@users.noreply.github.com>
2020-06-12 17:15:35 -0400

    added env

---
+++ next.config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+    env: {
+        DATABASE: process.env.DATABASE
+    }
+}
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb'
 
-const client = new MongoClient('mongodb+srv://mroglan:turtle44@cluster0-2fwso.mongodb.net/statpad', {
+const client = new MongoClient(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
