c96c2be7cef6f3bf13f7e235e0342126b6b27a62
mroglan <mroglan@users.noreply.github.com>
2020-06-30 21:20:41 -0400

    plz

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     env: {
         DATABASE: process.env.DATABASE,
         SIGNATURE: process.env.SIGNATURE,
-        API_ROUTE: process.env.API_ROUTE,
-        BASE_ROUTE: process.env.BASE_ROUTE
+        API_ROUTE: process.env.API_ROUTE2,
+        BASE_ROUTE: process.env.BASE_ROUTE2
     }
 }
