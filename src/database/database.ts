e19afcb4f023a1918ed01cab6a5d335d406e9129
mroglan <mroglan@users.noreply.github.com>
2020-06-30 21:43:32 -0400

    attempt to connect to db

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb'
 
-const client = new MongoClient(process.env.DATABASE, {
+const client = new MongoClient(`${process.env.DATABASE}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
