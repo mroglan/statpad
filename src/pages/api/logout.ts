0bffa1b4a8a769c063981e0882b86b13eed01256
mroglan <mroglan@users.noreply.github.com>
2020-06-30 22:09:27 -0400

    updated logout

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -6,7 +6,7 @@ export default function Logout(req: NextApiRequest, res: NextApiResponse) {
     res.setHeader('Set-Cookie', cookie.serialize('auth', 'bye bye', {
         maxAge: 0,
         path: '/',
-        httpOnly: true,
+        //httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
     }))
